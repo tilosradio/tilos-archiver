880db654e3590981c62a385b203e75bd124220a4
Janos Gergely <gergelyjanos@td1990.hu>
2023-07-14 16:38:59 +0200

    correct folders

--- install.sh
+++ install.sh
@@ -12,6 +12,9 @@ chmod uga+x /home/arcjob/bin/tilos_arc_cron_job.sh
 chmod uga+x /home/arcjob/bin/tilos_arc_mp3_maker.sh
 chmod uga-w /home/arcjob/bin/tilos_arc_cron_job.sh
 chmod uga-w /home/arcjob/bin/tilos_arc_mp3_maker.sh
+set +x
+
+crontab -l
 
 echo "crontab -e"
 more cron.txt
