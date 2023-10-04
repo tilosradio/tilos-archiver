dbefe1db89e29fca0a29e40713eb38ca9a5e25a3
Janos Gergely <gergelyjanos@td1990.hu>
2023-10-04 17:15:34 +0200

    fix pid

--- scripts/tilos_arc_cron_job.sh
+++ scripts/tilos_arc_cron_job.sh
@@ -1,5 +1,5 @@
 echo "$0"
-PID = "$$"
+PID="$$"
 LOG="/home/arcjob/logs/cron_$PID_`date -u +"%Y%m%d_%H%M%S"_utc.log`"
 echo "`date -u +"%Y%m%d_%H%M%S"` $USER begin:   $LOG" >> /home/arcjob/logs/jobs.log
 /home/arcjob/bin/tilos-archiver/active/scripts/tilos_arc_mp3_maker.sh $1 $2 > "$LOG" 2>&1
