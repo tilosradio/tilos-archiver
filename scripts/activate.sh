bb8aaaed7d49304575140a446c0a606c00388164
Janos Gergely <gergelyjanos@td1990.hu>
2023-10-05 16:28:01 +0200

    monitor script digital prefix

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -1,2 +1,6 @@
+set -x
 VERS=`cat ../VERSION`
+rm "/home/arcjob/bin/tilos-archiver/active"
 ln -s -f "/home/arcjob/bin/tilos-archiver/$VERS" "/home/arcjob/bin/tilos-archiver/active"
+ls -la "/home/arcjob/bin/tilos-archiver"
+set +x
---
+++ scripts/monitor.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+date -u
+cat ~/bin/tilos-archiver/active/VERSION
+df -h | grep disk_sg20t
+crontab -l | grep tilos
+ls -la ~/capture
+
+echo "process              count"
+echo " arcjob              `ps -ef | grep -v grep | grep arcjob -c`"
+echo " tilos_arc_cron_job  `ps -ef | grep -v grep | grep tilos_arc_cron -c`"
+echo " tilos_arc_mp3_maker `ps -ef | grep -v grep | grep tilos_arc_mp3 -c`"
+echo " fmedia              `ps -ef | grep -v grep | grep fmedia -c`"
+
+if [ "$1" = "-v" ]; then
+    echo "arcjob"
+    ps -ef | grep -v grep | grep -v "ps -ef" | grep arcjob
+    echo "tilos_arc_cron_job"
+    ps -ef | grep -v grep | grep tilos_arc_cron_job
+    echo "tilos_arc_mp3_maker"
+    ps -ef | grep -v grep | grep tilos_arc_mp3_maker
+    echo "fmedia"
+    ps -ef | grep -v grep | grep fmedia
+fi
